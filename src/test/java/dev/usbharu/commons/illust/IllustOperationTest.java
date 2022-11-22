af1e7fa9bec5931d1d1b7d3b12ad4dac2950fd93
usbharu <64310155+usbharu@users.noreply.github.com>
2022-11-22 21:50:38 +0900

    test: 複数回取得するテストを追加

--- src/test/java/dev/usbharu/commons/illust/IllustOperationTest.java
+++ src/test/java/dev/usbharu/commons/illust/IllustOperationTest.java
@@ -2,8 +2,11 @@ package dev.usbharu.commons.illust;
 
 import dev.usbharu.commons.illust.metadata.Metadata;
 import dev.usbharu.commons.illust.metadata.MetadataValue;
+import dev.usbharu.commons.illust.metadata.Tag;
+import dev.usbharu.commons.illust.metadata.Taggable;
 import dev.usbharu.commons.illust.metadata.type.Stringable;
 import java.io.File;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class IllustOperationTest {
@@ -17,6 +20,22 @@ class IllustOperationTest {
       if (data instanceof Stringable) {
         System.out.println(((Stringable) data).getStringValue());
       }
+    }
+  }
+
+  @Test
+  void aaa() {
+    for (int i = 0; i < 3; i++) {
+      long l = System.currentTimeMillis();
+      for (File file : new File("K:\\資料\\趣味\\クッキークリッカー\\しょぼいマフィンベーカリー").listFiles(
+          (dir, name) -> name.endsWith(".jpg"))) {
+        Metadata metadata = IllustOperation.getIllust(file).getMetadata();
+        if (metadata instanceof Taggable) {
+          List<? extends Tag> allTag = ((Taggable) metadata).getAllTag();
+        }
+      }
+      long l2 = System.currentTimeMillis();
+      System.out.println(l2 - l);
     }
   }
 }
