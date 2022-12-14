c3ca89f0add82b3df80d1373c7c359e2684eafd1
usbharu <64310155+usbharu@users.noreply.github.com>
2022-12-14 18:54:40 +0900

    style: importを削除

--- src/main/java/dev/usbharu/commons/illust/parser/impl/jpeg/xmp/XmpPropertyParser.java
+++ src/main/java/dev/usbharu/commons/illust/parser/impl/jpeg/xmp/XmpPropertyParser.java
@@ -1,8 +1,6 @@
 package dev.usbharu.commons.illust.parser.impl.jpeg.xmp;
 
 import com.adobe.internal.xmp.XMPMeta;
-import com.adobe.internal.xmp.options.PropertyOptions;
-import com.adobe.internal.xmp.properties.XMPProperty;
 import com.adobe.internal.xmp.properties.XMPPropertyInfo;
 import dev.usbharu.commons.illust.metadata.MetadataValue;
 import java.util.List;
