8e80a0290cf9d629da8064377403ffa0a19818cc
usbharu <64310155+usbharu@users.noreply.github.com>
2023-01-01 10:56:45 +0900

    test: リソースのパスを変更

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -10,7 +10,7 @@ public class MainTest {
   @Test
   void name() {
     Illust illust = IllustOperation.getIllust(
-        new File(getClass().getResource("123456_p1.png").getFile()));
+        new File(getClass().getResource("metadata/123456_p1.png").getFile()));
     List<? extends MetadataValue> allMetadata = illust.getMetadata().getAllMetadata();
     for (MetadataValue allMetadatum : allMetadata) {
       System.out.println(allMetadatum.getValue());
