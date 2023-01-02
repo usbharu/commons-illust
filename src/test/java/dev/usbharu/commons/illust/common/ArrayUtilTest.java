4c24a101d3b5d7e81afd65bdb5e83c21ee31db1e
usbharu <64310155+usbharu@users.noreply.github.com>
2023-01-02 10:23:34 +0900

    test: ArrayUtilTestに配列の範囲が同じかのテストを追加

--- src/test/java/dev/usbharu/commons/illust/common/ArrayUtilTest.java
+++ src/test/java/dev/usbharu/commons/illust/common/ArrayUtilTest.java
@@ -2,10 +2,15 @@ package dev.usbharu.commons.illust.common;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 
 class ArrayUtilTest {
@@ -23,5 +28,46 @@ class ArrayUtilTest {
     } catch (InstantiationException | IllegalAccessException e) {
       throw new RuntimeException(e);
     }
+  }
+
+  @ParameterizedTest
+  @MethodSource("equals_indexes_InRangeAndTheSameValue_returnTrue_Source")
+  void equals_indexesInRangeAndTheSameValue_returnTrue(byte[] a,
+      int aFromIndex,
+      int aToIndex,
+      byte[] b,
+      int bFromIndex,
+      int bToIndex) {
+    assertTrue(ArrayUtil.equals(a, aFromIndex, aToIndex, b, bFromIndex, bToIndex));
+  }
+
+
+  static Stream<Arguments> equals_indexes_InRangeAndTheSameValue_returnTrue_Source() {
+    return Stream.of(
+        arguments(
+            createByteArrayFromIntArray(808, 800, 890, 238, 913, 749), 0, 3,
+            createByteArrayFromIntArray(808, 800, 890, 691, 581, 444), 0, 3),
+        arguments(createByteArrayFromIntArray(659, 427, 953, 863, 310, 490, 309, 952), 3, 6,
+            createByteArrayFromIntArray(478, 652, 863, 310, 490), 2, 5)
+    );
+  }
+
+  @ParameterizedTest
+  @MethodSource
+  void equals_indexesInRangeAndTheDifferentValue_returnFalse(byte[] a,
+      int aFromIndex,
+      int aToIndex,
+      byte[] b,
+      int bFromIndex,
+      int bToIndex) {
+    assertFalse(ArrayUtil.equals(a, aFromIndex, aToIndex, b, bFromIndex, bToIndex));
+  }
+
+  private static byte[] createByteArrayFromIntArray(int... array) {
+    byte[] bytes = new byte[array.length];
+    for (int i = 0; i < array.length; i++) {
+      bytes[i] = (byte) array[i];
+    }
+    return bytes;
   }
 }
