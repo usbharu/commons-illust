976054fda21b513b15660e32253413e2e733a9db
usbharu <64310155+usbharu@users.noreply.github.com>
2023-01-03 14:33:30 +0900

    test: IllustSourceのテストを追加

--- src/test/java/dev/usbharu/commons/illust/common/IllustSourceTest.java
+++ src/test/java/dev/usbharu/commons/illust/common/IllustSourceTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.IOException;
 import java.io.InputStream;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 abstract class IllustSourceTest {
